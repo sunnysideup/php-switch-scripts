1324b1b3900231552f56f1252bd415f77d3abf00
Nicolaas @ Tappy @ Sunny Side Up <github@sunnysideup.co.nz>
2023-02-02 20:38:55 +1300

    WIP

--- setup.sh
+++ setup.sh
@@ -84,7 +84,7 @@ sudo apt-get install -y php8.1 php8.1-common php8.1-cli > /dev/null
 
 
 echo "* Installing PHP 8.1 extensions..."
-sudo apt-get install -y php8.1-bz2 php8.1-curl php8.1-gd php8.1-json php8.1-mbstring php8.1-mysql php8.1-opcache php8.1-readline php8.1-soap php8.1-sqlite3 php8.1-tidy php8.1-xml php8.1-xsl php8.1-zip > /dev/null
+sudo apt-get install -y php8.1-bz2 php8.1-curl php8.1-intl php8.1-gd php8.1-json php8.1-mbstring php8.1-mysql php8.1-opcache php8.1-readline php8.1-soap php8.1-sqlite3 php8.1-tidy php8.1-xml php8.1-xsl php8.1-zip > /dev/null
 
 
 echo "* Installing additional PHP extensions..."
