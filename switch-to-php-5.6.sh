ab38b621a4989621434915bc4b33b0fb8b7a4752
Nicolaas @ Tappy <nt@sunnysideup.co.nz>
2026-05-13 08:18:27 +1200

    FIX: cleanup

--- switch-to-php-5.6.sh
+++ switch-to-php-5.6.sh
@@ -1,45 +1,51 @@
 #!/bin/bash
 
-echo "* Disabling Apache PHP 5.6 module..."
-sudo a2dismod php5.6> /dev/null
+# echo "* Disabling Apache PHP 5.6 module..."
+# sudo a2dismod php5.6 >/dev/null
 
 echo "* Disabling Apache PHP 7.0 module..."
-sudo a2dismod php7.0 > /dev/null
+sudo a2dismod php7.0 >/dev/null
 
 echo "* Disabling Apache PHP 7.1 module..."
-sudo a2dismod php7.1 > /dev/null
+sudo a2dismod php7.1 >/dev/null
 
 echo "* Disabling Apache PHP 7.2 module..."
-sudo a2dismod php7.2 > /dev/null
+sudo a2dismod php7.2 >/dev/null
 
 echo "* Disabling Apache PHP 7.3 module..."
-sudo a2dismod php7.3 > /dev/null
+sudo a2dismod php7.3 >/dev/null
 
 echo "* Disabling Apache PHP 7.4 module..."
-sudo a2dismod php7.4 > /dev/null
+sudo a2dismod php7.4 >/dev/null
 
 echo "* Disabling Apache PHP 8.0 module..."
-sudo a2dismod php8.0 > /dev/null
+sudo a2dismod php8.0 >/dev/null
 
 echo "* Disabling Apache PHP 8.1 module..."
-sudo a2dismod php8.1 > /dev/null
+sudo a2dismod php8.1 >/dev/null
 
 echo "* Disabling Apache PHP 8.2 module..."
-sudo a2dismod php8.2 > /dev/null
+sudo a2dismod php8.2 >/dev/null
 
 echo "* Disabling Apache PHP 8.3 module..."
-sudo a2dismod php8.3> /dev/null
+sudo a2dismod php8.3 >/dev/null
+
+echo "* Disabling Apache PHP 8.4 module..."
+sudo a2dismod php8.4 >/dev/null
+
+echo "* Disabling Apache PHP 8.5 module..."
+sudo a2dismod php8.5 >/dev/null
 
 ####################################
 
 echo "* Enabling Apache PHP 5.6 module..."
-sudo a2enmod php5.6 > /dev/null
+sudo a2enmod php5.6 >/dev/null
 
 echo "* Restarting Apache..."
-sudo service apache2 restart > /dev/null
+sudo service apache2 restart >/dev/null
 
 echo "* Switching CLI PHP to 5.6..."
-sudo update-alternatives --set php /usr/bin/php5.6 > /dev/null
+sudo update-alternatives --set php /usr/bin/php5.6 >/dev/null
 
 echo "* Switch to PHP 5.6 complete."
 
--- switch-to-php-8.5.sh
+++ switch-to-php-8.5.sh
@@ -12,8 +12,8 @@ sudo a2dismod php7.1 >/dev/null
 echo "* Disabling Apache PHP 7.2 module..."
 sudo a2dismod php7.2 >/dev/null
 
-# echo "* Disabling Apache PHP 7.3 module..."
-# sudo a2dismod php7.3 >/dev/null
+echo "* Disabling Apache PHP 7.3 module..."
+sudo a2dismod php7.3 >/dev/null
 
 echo "* Disabling Apache PHP 7.4 module..."
 sudo a2dismod php7.4 >/dev/null
@@ -33,19 +33,21 @@ sudo a2dismod php8.3 >/dev/null
 echo "* Disabling Apache PHP 8.4 module..."
 sudo a2dismod php8.4 >/dev/null
 
-echo "* Disabling Apache PHP 8.5 module..."
-sudo a2dismod php8.5 >/dev/null
+# echo "* Disabling Apache PHP 8.5 module..."
+# sudo a2dismod php8.5 >/dev/null
+
 ####################################
-echo "* Enabling Apache PHP 7.3 module..."
-sudo a2enmod php7.3 >/dev/null
+
+echo "* Enabling Apache PHP 8.5 module..."
+sudo a2enmod php8.5 >/dev/null
 
 echo "* Restarting Apache..."
 sudo service apache2 restart >/dev/null
 
-echo "* Switching CLI PHP to 7.3..."
-sudo update-alternatives --set php /usr/bin/php7.3 >/dev/null
+echo "* Switching CLI PHP to 8.5..."
+sudo update-alternatives --set php /usr/bin/php8.5 >/dev/null
 
-echo "* Switch to PHP 7.3 complete."
+echo "* Switch to PHP 8.5 complete."
 
 sudo service apache2 restart
 sudo -u www-data php -v
